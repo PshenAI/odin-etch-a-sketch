fba6e4b8fa5815c285713a81ac7c4fe8e01d6207
pshenai <antonvoloshyn25@gmail.com>
2024-05-05 11:19:54 +0300

    Add basic logic for Etch'a'sketch
    Add index.html, styles.css and script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+const DEFAULT_SQUARE_NUM = 16;
+drawGrid(DEFAULT_SQUARE_NUM);
+
+const newSizeButton = document.querySelector('#new-size');
+newSizeButton.addEventListener('click', () =>{
+    let newSize = prompt('Enter new grid size from 16 to 100: ');
+
+    if(newSize > 100 || newSize < 16) {
+        newSize = prompt('Enter valid number: ');
+    }
+
+    clearGrid();
+
+    drawGrid(newSize);
+})
+
+const resetGridButton = document.querySelector('#reset');
+resetGridButton.addEventListener('click', resetGrid)
+
+function resetGrid() {
+    clearGrid();
+
+    drawGrid(DEFAULT_SQUARE_NUM);
+}
+
+function clearGrid() {
+    const grid = document.querySelector('.grid');
+    grid.remove();
+}
+
+function drawGrid(size) {
+    const container = document.querySelector('.container');
+
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+
+    for (let row = 0; row < size; row++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        row.style.cssText = `height: calc(100%/${size});`;
+        for (let i = 0; i < size; i++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.style.cssText = `width: calc(100%/${size});`;
+
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = 'black';
+            })
+
+            row.appendChild(square);
+        }
+        grid.appendChild(row);
+    }
+
+    container.appendChild(grid);
+}
+
+
